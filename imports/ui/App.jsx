be4331da5f58f154fc39382dbe342f43d2e102cc
S1B41 <sibai@pm.me>
2020-04-02 18:35:47 +0200

    add bootstrap

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Note from './Note';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const App = () => (
   <div>
     <Note />
+    <button className="btn btn-outline-success">Hello</button>
   </div>
 );
