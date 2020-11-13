9ff6544914bc35e6e186124be3a8ee8cb8b431ef
S1B41 <sibai@pm.me>
2020-11-13 11:35:10 +0100

    add logo + fix navbar

--- imports/ui/navbar.jsx
+++ imports/ui/navbar.jsx
@@ -7,11 +7,19 @@ export default () => {
         display: "flex",
         flexDirection: "row",
         justifyContent: "space-between",
+        alignItems: "center",
         backgroundColor: "#4995C7",
         height: 50,
+        color: "#fff",
+        fontWeight: 700,
+        fontSize: "26px",
+        padding: "0 30px",
       }}
     >
-      <div>Listo</div>
+      <div>
+        <img src="/listo-logo.png" width="40" />
+        <span style={{ marginLeft: 10 }}>Listo</span>
+      </div>
       <div>User</div>
     </div>
   );
