ece446794bdeddba14918173a9d06f99c193a631
S1B41 <sibai@pm.me>
2020-11-15 16:21:57 +0100

    remove unneeded insert

--- server/main.js
+++ server/main.js
@@ -1,8 +1,3 @@
 import { Meteor } from "meteor/meteor";
-import Notes from "/imports/collections/notes";
 
-Meteor.startup(() => {
-  if (Notes.find().count() === 0) {
-    Notes.insert({ name: "test", createdAt: new Date() });
-  }
-});
+Meteor.startup(() => {});
