6790a0f9bcd0c73ea605a618ab5d0ba9e8750902
S1B41 <sibai@pm.me>
2020-11-15 16:10:57 +0100

    cleanup

--- server/main.js
+++ server/main.js
@@ -1,20 +1,8 @@
-import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/collections/links';
-import Notes from '/imports/collections/notes';
-
-function insertLink({ title, url }) {
-  LinksCollection.insert({ title, url, createdAt: new Date() });
-}
+import { Meteor } from "meteor/meteor";
+import Notes from "/imports/collections/notes";
 
 Meteor.startup(() => {
-  // If the Links collection is empty, add some data.
-  if (LinksCollection.find().count() === 0) {
-    insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://www.meteor.com/tutorials/react/creating-an-app'
-    });
-  }
   if (Notes.find().count() === 0) {
-    Notes.insert({ name: 'test', createdAt: new Date() });
+    Notes.insert({ name: "test", createdAt: new Date() });
   }
 });
